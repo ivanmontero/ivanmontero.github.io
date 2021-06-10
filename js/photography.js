4eb06639caa572ea77e595927284ef23f17b2df7
Ivan Montero <ivanspmontero@gmail.com>
2021-06-09 19:19:34 -0700

    Add initial image gallery

---
+++ js/photography.js
@@ -0,0 +1,12 @@
+var folder = "img/photography/";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("#images").append("<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
